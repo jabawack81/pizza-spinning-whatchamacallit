8c6a20b53b7f1ab848353f2a5ba8e1d552f40854
Paolo Fabbri <jabawack81@gmail.com>
2022-06-25 00:29:20 +0100

    add basic auth

--- c2e/app.js
+++ c2e/app.js
@@ -4,7 +4,7 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import 'dotenv/config'
-import basicAuth  from'express-basic-auth';
+import * as basicAuth from 'express-basic-auth'
 
 
 import indexRouter from './routes/index.js';
