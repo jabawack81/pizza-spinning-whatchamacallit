de554b474aa0042a3474af7836d5d8b9fe8d7bdc
Paolo Fabbri <jabawack81@gmail.com>
2022-06-28 22:32:53 +0100

    add homepage animation

--- c2e/public/index.js
+++ c2e/public/index.js
@@ -1,7 +1,19 @@
 $( document ).ready(function() {
-  $("#forward").click((e) => doTheFetch("forward") )
-  $("#backward").click((e) => doTheFetch("backward") )
-  $("#stop").click((e) => doTheFetch("stop") )
+  $("#forward").click((e) => {
+    doTheFetch("forward")
+    $("img").addClass("forward");
+    $("img").removeClass("backward");
+  })
+  $("#backward").click((e) => {
+    doTheFetch("backward")
+    $("img").removeClass("forward");
+    $("img").addClass("backward");
+  })
+  $("#stop").click((e) => {
+    doTheFetch("stop")
+    $("img").removeClass("forward");
+    $("img").removeClass("backward");
+  })
   console.log( "ready!" );
 });
 const doTheFetch = (endpoint) => {
